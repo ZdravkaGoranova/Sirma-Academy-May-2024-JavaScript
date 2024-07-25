39d0583d990f2c7c5654d86b33fb92461091cfc5
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2024-07-26 00:18:25 +0300

    refactor: update advanced-functions9.js Advanced Functions

--- II.Sirma Academy Professional Program/22. Advanced Functions/advanced-functions9.js
+++ II.Sirma Academy Professional Program/22. Advanced Functions/advanced-functions9.js
@@ -6,13 +6,15 @@ function getFibonacci() {
   let n1 = 0;
   let n2 = 1;
   let nextTerm;
-  
-  return function () {
+
+  let result = function () {
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
     return n1;
   };
+  
+  return result;
 }
 
 let fibonacci = getFibonacci();
